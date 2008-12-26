dc0d482d6affcf4e3c1863adb39b7247e6d04b90
Luis Lavena <luislavena@gmail.com>
2008-12-26 04:21:23 -0200

    Added first Package specs and code.

--- lib/one_click/package.rb
+++ lib/one_click/package.rb
@@ -1,4 +1,17 @@
 module OneClick
   class Package
+    attr_accessor :name
+    attr_accessor :version
+
+    def initialize(name = nil, version = nil, &block)
+      @name = name
+      @version = version
+      define if block_given?
+    end
+
+    def define
+      fail 'package name is required' if @name.nil?
+      fail 'package version is required' if @version.nil?
+    end
   end
 end
--- spec/lib/one_click/package_spec.rb
+++ spec/lib/one_click/package_spec.rb
@@ -1,4 +1,39 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 describe OneClick::Package do
+  describe "#new" do
+    it 'should fail if no name for the package was given' do
+      lambda {
+        OneClick::Package.new { }
+      }.should raise_error(RuntimeError, /package name is required/)
+    end
+
+    it 'should fail if no version for the package was given' do
+      lambda {
+        OneClick::Package.new('foo') { }
+      }.should raise_error(RuntimeError, /package version is required/)
+    end
+
+    it 'should not fail if both name and version for the package was given' do
+      lambda {
+        OneClick::Package.new('foo', '1.2.3') { }
+      }.should_not raise_error(RuntimeError)
+    end
+
+    it 'should not attempt to define package if no block was provided' do
+      lambda {
+        OneClick::Package.new
+      }.should_not raise_error(RuntimeError)
+    end
+
+    it 'should allow package name be accessed' do
+      pkg = OneClick::Package.new('foo', '1.2.3')
+      pkg.name.should == 'foo'
+    end
+
+    it 'should allow package version be accessed' do
+      pkg = OneClick::Package.new('foo', '1.2.3')
+      pkg.version.should == '1.2.3'
+    end
+  end
 end
