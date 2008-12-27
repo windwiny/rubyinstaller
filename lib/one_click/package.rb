9bd53fbd57ef5ac604b006162b9923e24754d363
Luis Lavena <luislavena@gmail.com>
2008-12-27 03:45:36 -0200

    Defined download tasks for Package.

--- lib/one_click/package.rb
+++ lib/one_click/package.rb
@@ -19,9 +19,29 @@ module OneClick
       end
     end
 
+    def pkg_dir
+      @pkg_dir ||= File.join(OneClick.sandbox_dir, @name, @version)
+    end
+
     def define
       fail 'package name is required' if @name.nil?
       fail 'package version is required' if @version.nil?
+
+      Rake::Task.define_task("#{@name}:#{@version}")
+      Rake::Task["#{@name}:#{@version}"].comment = "Build version #{@version} of #{@name}"
+    end
+
+    def define_download
+      return unless @actions.has_downloads?
+
+      @actions.downloads.each do |download|
+        Rake::FileTask.define_task("#{pkg_dir}/#{download[:file]}") do |t|
+          OneClick::Utils.download(download[:url], pkg_dir)
+        end
+
+        # package:version:download => [sandbox/package/version/file]
+        Rake::Task.define_task("#{@name}:#{@version}:download" => ["#{pkg_dir}/#{download[:file]}"])
+      end
     end
   end
 end
--- spec/lib/one_click/package_spec.rb
+++ spec/lib/one_click/package_spec.rb
@@ -66,4 +66,71 @@ describe OneClick::Package do
       pkg.actions.should be_nil
     end
   end
+
+  describe '#pkg_dir' do
+    before :each do
+      @pkg = OneClick::Package.new('foo', '4.5.6')
+    end
+
+    it 'should use OneClick sandbox definition' do
+      OneClick.should_receive(:sandbox_dir).and_return('something')
+      @pkg.pkg_dir
+    end
+
+    it 'should define a folder structure using name and version' do
+      OneClick.stub!(:sandbox_dir).and_return('something')
+      @pkg.pkg_dir.should == 'something/foo/4.5.6'
+    end
+  end
+
+  describe "#define" do
+    predicate_matchers[:have_defined] = :task_defined?
+
+    before :each do
+      Rake.application.clear
+
+      @mock_actions = mock(OneClick::Package::Actions)
+      OneClick::Package::Actions.stub!(:new).and_return(@mock_actions)
+      @pkg = OneClick::Package.new('foo', '4.5.6')
+    end
+
+    it 'should define a task for the package version' do
+      Rake::Task.should_not have_defined('foo:4.5.6')
+      @pkg.define
+      Rake::Task.should have_defined('foo:4.5.6')
+    end
+
+    describe "#define_download" do
+      before :each do
+        @files = [{:file => 'foo-4.5.6.zip', :url => 'http://www.domain.com/foo-4.5.6.zip'}]
+        OneClick.stub!(:sandbox_dir).and_return('tmp')
+        @mock_actions.stub!(:has_downloads?).and_return(true)
+        @mock_actions.stub!(:downloads).and_return(@files)
+      end
+
+      it 'should not define task if package has no downloads' do
+        @mock_actions.should_receive(:has_downloads?).and_return(false)
+        @pkg.define_download
+        Rake::Task.should_not have_defined('foo:4.5.6:download')
+      end
+
+      it 'should define the package download task' do
+        @mock_actions.should_receive(:has_downloads?).and_return(true)
+        @mock_actions.should_receive(:downloads).and_return(@files)
+        @pkg.define_download
+        Rake::Task.should have_defined('foo:4.5.6:download')
+      end
+
+      it 'should define file tasks for each downloadable' do
+        Rake::Task.should_not have_defined('tmp/foo/4.5.6/foo-4.5.6.zip')
+        @pkg.define_download
+        Rake::Task.should have_defined('tmp/foo/4.5.6/foo-4.5.6.zip')
+      end
+
+      it 'should make the file tasks part of the download one' do
+        @pkg.define_download
+        Rake::Task['foo:4.5.6:download'].prerequisites.should include('tmp/foo/4.5.6/foo-4.5.6.zip')
+      end
+    end
+  end
 end
