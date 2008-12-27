f089b25e9ef3feb5397cf742193b7c76a2a1dc25
Luis Lavena <luislavena@gmail.com>
2008-12-27 15:10:07 -0200

    Reverted API for Package::Actions.
    (Still not convinced on the syntax).

--- lib/one_click/package/actions.rb
+++ lib/one_click/package/actions.rb
@@ -4,12 +4,9 @@ module OneClick
       autoload :URI, 'uri'
 
       attr_reader :downloads
-      attr_reader :dependencies
 
       def initialize
         @downloads = []
-        @tools = []
-        @dependencies = []
       end
 
       def download(url)
@@ -19,22 +16,6 @@ module OneClick
 
       def has_downloads?
         @downloads.size > 0
-      end
-
-      def uses(tool)
-        @tools << tool unless uses?(tool)
-      end
-
-      def uses?(tool)
-        @tools.include?(tool)
-      end
-
-      def depends_on(package, options = {})
-        @dependencies << { :package => package }.merge(options)
-      end
-
-      def has_dependencies?
-        @dependencies.size > 0
       end
     end
   end
